ba66287dd84fee92d393a6e178adffcaba07a4d4
NataliaArsh <nata-arsh@mail.ru>
2022-03-22 10:58:18 +0300

    add PostItem

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,30 +1,29 @@
 <template>
-  <div class="post" v-for="post in posts">
-    <div class="post__title"><strong>Название:</strong> {{ post.title }}</div>
-    <div class="post__body"><strong>Описание:</strong> {{ post.body }}</div>
+  <div>
+    <h3>Список постов</h3>
+    <post-item
+      v-for="post in posts"
+      :post="post"
+    /> 
   </div>
 </template>
 
 <script>
+import PostItem from "@/components/PostItem";
 export default {
-    props: {
-      posts: {
-        type: Array,
-        recuired: true
-      }
+  components: {
+    PostItem
+  },
+  props: {
+    posts: {
+      type: Array,
+      recuired: true
     }
+  }
 }
 </script>
 
 
 <style scoped>
-.post {
-  border: 2px solid rgb(224, 87, 224);
-  margin: 10px 0;
-  padding: 10px;
-}
 
-.post__title {
-  margin-bottom: 5px;
-}
 </style>
