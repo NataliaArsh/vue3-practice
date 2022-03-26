a917bfad8d0e7a64b7cfa787f0822ad053b98012
NataliaArsh <nata-arsh@mail.ru>
2022-03-26 06:04:32 +0300

    add MyInput

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -1,15 +1,15 @@
 <template>
   <form class="form" @submit.prevent>
-    <input 
-      type="text" 
+    <my-input
       placeholder="Введите название"
       v-model="post.title"
-    >
-    <input 
-      type="text" 
+    >  
+    </my-input> 
+    <my-input
       placeholder="Введите описание"
       v-model="post.body"
-    >
+    >    
+    </my-input> 
     <my-button
       @click="addPost"
       class="btn-form"
@@ -46,15 +46,6 @@ export default {
 form {
   display: flex;
   flex-direction: column;
-}
-
-input {
-  display: inline-block;
-  width: 100%;
-  height: 35px;
-  border: 2px solid rgb(224, 87, 224);
-  margin-bottom: 10px;
-  padding-left: 5px;
 }
 
 .btn-form {
---
+++ src/components/UI/MyInput.vue
@@ -0,0 +1,22 @@
+<template>
+    <input type="text" class="input">
+</template>
+
+<script>
+export default {
+  name: 'my-input'
+}
+</script>
+
+
+<style scoped>
+.input {
+  display: inline-block;
+  width: 100%;
+  height: 35px;
+  border: 2px solid rgb(224, 87, 224);
+  margin-bottom: 10px;
+  padding-left: 5px;
+}
+
+</style>
